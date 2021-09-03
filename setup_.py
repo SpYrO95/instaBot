7d6973c7e3282acaa144cd90c082aace03bdbcab
SpYrO95 <emmanuel.lanzino@gmail.com>
2021-09-03 19:29:31 +0200

    Automation Python Application For Automate Instagram Iteration

---
+++ setup_.py
@@ -0,0 +1,19 @@
+from post_list import PostList
+
+POST_HOUR = 12
+POST_DAYS = ["sunday", "thursday", "wednesday"]
+POST_LIST = PostList([])
+DATES_OF_POSTS = {}
+
+USERNAME = "spyr095"
+PASSWORD = "ferrarelle"
+
+DAYS_MAP = {
+    0: "monday",
+    1: "tuesday",
+    2: "wednesday",
+    3: "thursday",
+    4: "friday",
+    5: "saturday",
+    6: "sunday",
+}
